ff30afddf04e1919f6d6c945ae15b7113bbe940e
Jas <webmaster@jaspaul.com>
2024-05-15 21:45:42 +0100

    Details component

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -1,11 +1,13 @@
-// Details component
-// https://design-system.service.gov.uk/components/details/
 import type { ComponentProps } from 'react'
 
 type Props = {
   title: string
 } & ComponentProps<'details'>
 
+/**
+ * Renders a details component with a summary and text content.
+ * https://design-system.service.gov.uk/components/details/
+ */
 export default function Details({ title, className, children, ...props }: Props) {
   return (
     <details className={`govuk-details ${className || ''}`.trim()} {...props}>

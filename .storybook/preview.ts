409e242aca59d45e8643b48d53c1987153a46d3e
Jas <webmaster@jaspaul.com>
2024-05-15 22:00:45 +0100

    preview viewport update

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,16 @@
 import type { Preview } from '@storybook/react'
+import { INITIAL_VIEWPORTS, MINIMAL_VIEWPORTS } from '@storybook/addon-viewport'
 
 const preview: Preview = {
   tags: ['autodocs'],
   parameters: {
+    layout: 'fullscreen',
+    viewport: {
+      viewports: {
+        ...INITIAL_VIEWPORTS,
+        ...MINIMAL_VIEWPORTS
+      }
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,

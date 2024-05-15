69d32497a87e3bdce5205e94dc20ba29f8947fe9
Jas <webmaster@jaspaul.com>
2024-05-15 16:24:18 +0100

    first version of unit test.

---
+++ src/setupTests.ts
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom'
+
+/*
+snippet if need to use fetchMock later
+import { vi } from 'vitest'
+import createFetchMock from 'vitest-fetch-mock'
+const fetchMocker = createFetchMock(vi)
+
+// adds the 'fetchMock' global variable and rewires 'fetch' global to call 'fetchMock' instead of the real implementation
+fetchMocker.enableMocks()
+*/

dbd3c1d28eafc2f027d95b9abbf80b5fa283c70c
Jas <webmaster@jaspaul.com>
2024-05-15 21:21:34 +0100

    Details component

---
+++ src/components/Details/Details.tsx
@@ -0,0 +1,18 @@
+// Details component
+// https://design-system.service.gov.uk/components/details/
+import type { ComponentProps } from 'react'
+
+type Props = {
+  title: string
+} & ComponentProps<'details'>
+
+export default function Details({ title, className, children, ...props }: Props) {
+  return (
+    <details className={`govuk-details ${className || ''}`.trim()} {...props}>
+      <summary className="govuk-details__summary">
+        <span className="govuk-details__summary-text">{title}</span>
+      </summary>
+      <div className="govuk-details__text">{children}</div>
+    </details>
+  )
+}
---
+++ src/const.ts
@@ -0,0 +1,46 @@
+/**
+ * UI constants for design attributes
+ * https://design-system.service.gov.uk/styles/
+ */
+
+export const sizes = {
+  small: 's',
+  medium: 'm',
+  large: 'l',
+  'extra-large': 'xl'
+} as const
+
+export const layout = {
+  full: 'full',
+  'three-quarters': 'three-quarters',
+  'two-thirds': 'two-thirds',
+  'one-half': 'one-half',
+  'one-third': 'one-third',
+  'one-quarter': 'one-quarter'
+} as const
+
+export const tagColours = {
+  grey: 'grey',
+  green: 'green',
+  turquoise: 'turquoise',
+  blue: 'blue',
+  light: 'light',
+  purple: 'purple',
+  pink: 'pink',
+  red: 'red',
+  orange: 'orange',
+  yellow: 'yellow'
+} as const
+
+/*
+export const statusColours = {
+  initiated: 'pink',
+  pending: 'blue',
+  granted: 'green',
+  declined: 'red',
+  cancelled: 'orange',
+  withdrawn: 'yellow',
+  'no results found': 'orange',
+  default: 'grey'
+} as const
+*/

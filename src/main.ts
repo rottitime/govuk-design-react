241471b8a3cfb2e03fa9f76f73f2bb2197d7b86a
Jas <webmaster@jaspaul.com>
2024-05-15 15:55:41 +0100

    working version of package

--- src/main.ts
+++
@@ -1,5 +0,0 @@
-export function helloAnything(thing: string): string {
-  return `Hello ${thing}!`
-}
-
-export { default as App } from './App'
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import path from 'path'
 import react from '@vitejs/plugin-react-swc'
 import dts from 'vite-plugin-dts'
 
@@ -7,11 +9,19 @@ export default defineConfig({
   plugins: [react(), dts()],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/main.ts'),
+      entry: path.resolve(__dirname, 'main.ts'),
       name: '@rottitime/govuk-design-react',
       fileName: (format) => `main.${format}.js`
     },
-    copyPublicDir: false,
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    },
     sourcemap: true,
     emptyOutDir: true
   }

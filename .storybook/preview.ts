1cee4847508050ad3ef677d310d52c07d63988a0
Jas Aggarwal <webmaster@jaspaul.com>
2024-05-16 17:23:42 +0100

    storybook govuk stylesheet fix (#6)
    * Details component
    
    * Details component
    
    * latest

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,8 @@
 import type { Preview } from '@storybook/react'
 import { INITIAL_VIEWPORTS, MINIMAL_VIEWPORTS } from '@storybook/addon-viewport'
 
+import 'govuk-frontend/dist/govuk/all.scss'
+
 const preview: Preview = {
   tags: ['autodocs'],
   parameters: {
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 
-// v4 test
+// v5 test
 
 function App() {
   const [count, setCount] = useState(0)

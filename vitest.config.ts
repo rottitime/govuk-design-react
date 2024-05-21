2212829a50ce4f72e9ca3451a0fdef6b9fdbca5d
Jas <webmaster@jaspaul.com>
2024-05-21 18:33:23 +0100

    alis integration to test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,7 @@
 // vitest.config.ts
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -10,5 +11,10 @@ export default defineConfig({
     include: ['**/?(*.)test.ts?(x)'],
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts'
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   }
 })

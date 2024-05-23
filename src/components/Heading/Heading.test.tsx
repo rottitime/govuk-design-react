bebbf512cd1749f1f5e03f5bd5ad3cc64d4ecc8a
Jas <webmaster@jaspaul.com>
2024-05-23 18:05:51 +0100

    unit tests completed

--- src/components/Heading/Heading.test.tsx
+++ src/components/Heading/Heading.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import Heading from './Heading'
 import type { sizes } from '@/const'
+import type { ComponentProps } from 'react'
 
 const text = 'Hello, world!'
 
@@ -10,7 +11,22 @@ describe('Heading', () => {
     expect(screen.getByRole('heading', { name: text })).toBeInTheDocument()
   })
 
-  test.each<[keyof typeof sizes, string]>([
+  it.each<[ComponentProps<typeof Heading>['level'], string]>([
+    [1, 'h1'],
+    [2, 'h2'],
+    [3, 'h3'],
+    [4, 'h4'],
+    [5, 'h5'],
+    [6, 'h6']
+  ])('renders the heading with level %s as %s', (level, expectedHeading) => {
+    render(<Heading level={level}>{text}</Heading>)
+    expect(screen.getByRole('heading', { level })).toBeInTheDocument()
+    expect(screen.getByRole('heading', { level }).tagName.toLowerCase()).toBe(
+      expectedHeading
+    )
+  })
+
+  it.each<[keyof typeof sizes, string]>([
     ['small', 'govuk-heading-s'],
     ['medium', 'govuk-heading-m'],
     ['large', 'govuk-heading-l'],

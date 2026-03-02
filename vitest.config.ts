941df3475d9d24f06f84e75befaed6b3b53b5b72
Jas <webmaster@jaspaul.com>
2026-03-02 15:53:03 +0000

    deprecate @vitejs/plugin-react-swc

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,15 @@
-// vitest.config.ts
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
-
   test: {
     globals: true,
     include: ['**/?(*.)test.ts?(x)'],
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts'
   },
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')

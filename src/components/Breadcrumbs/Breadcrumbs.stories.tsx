8c3e59169df315fa47d7d0996d40a450a1097a31
Ramesh Reddy Adutla <rameshreddy.adutla@gmail.com>
2026-03-05 12:42:52 +0000

    fix: update Figma URL and fix broken description string in Breadcrumbs stories

--- src/components/Breadcrumbs/Breadcrumbs.stories.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -9,12 +9,12 @@ const meta: Meta<typeof Breadcrumbs> = {
     docs: {
       description: {
         component:
-          'The breadcrumbs component helps users to understand where they are within a website's structure and move between levels. See https://design-system.service.gov.uk/components/breadcrumbs/ for more details.'
+          'The breadcrumbs component helps users to understand where they are within a website\'s structure and move between levels. See https://design-system.service.gov.uk/components/breadcrumbs/ for more details.'
       }
     },
     design: {
       type: 'figma',
-      url: 'https://www.figma.com/design/Uim7G5Td35hg5PTGQ79OA1/GOV.UK-Design-System--Community-?node-id=213-4&t=uKHtjFmxXYtHoZTO-0'
+      url: 'https://www.figma.com/design/Uim7G5Td35hg5PTGQ79OA1/GOV.UK-Design-System--Community-?node-id=20123-12775&t=iHqp2471YNPlJMvO-0'
     }
   }
 }
